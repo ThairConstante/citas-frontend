a2c8be12978b0ad2d0cbb85712dbd8bed564fd1f
ThairConstante <thairconstante123@gmail.com>
2025-06-04 15:17:48 -0500

    frontend

---
+++ const.js
@@ -0,0 +1,10 @@
+
+const ENV = false
+
+function getServerUrl(){
+    if(ENV){
+        return 'http://127.0.0.1:8000'
+    }else{
+        return 'https://citas-backend-uri4.onrender.com'
+    }
+}
